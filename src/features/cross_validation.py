df5cc9820159f9130271f03f7d5d1607fbfc00dc
saritepe <ugursaritepe@gmail.com>
2020-10-14 16:59:36 +0300

    cross_validation.py and cv_array (RepatedKFold and RepeatedStratifiedKFold) added to global_parameters.yaml. There are more CV options to be added.

---
+++ src/features/cross_validation.py
@@ -0,0 +1,5 @@
+from sklearn import model_selection
+
+
+def select_cv(cv_name, n_splits=5, n_repeats=3, random_state=42):
+    return getattr(model_selection, cv_name)(n_splits=n_splits, n_repeats=n_repeats, random_state=random_state)
