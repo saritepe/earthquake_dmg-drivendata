4b2cfa732e61c19c8114865e69bde3a0d47fe2a6
saritepe <ugursaritepe@gmail.com>
2020-10-14 10:07:40 +0300

    load_data.py default parameters changed. File paths changed at file_paths.yaml

--- src/data/load_data.py
+++ src/data/load_data.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 
-def separated_initial_data_load(values_file_path='../data/raw/train_values.csv',
-                                labels_file_path='../data/raw/train_labels.csv', sep=','):
+def separated_initial_data_load(values_file_path='data/raw/train_values.csv',
+                                labels_file_path='data/raw/train_labels.csv', sep=','):
     values = pd.read_csv(values_file_path, sep=sep)
     labels = pd.read_csv(labels_file_path, sep=sep)
     return values.merge(labels)
--- test_funcs.py
+++ test_funcs.py
@@ -1,5 +1,12 @@
 from src.data.categorize_data import get_cat_cols
 from src.data.categorize_data import get_drop_cols
+from src.data.load_data import separated_initial_data_load
+from src.utils.get_config import get_config
+
 
 print(get_cat_cols())
 print(get_drop_cols())
+
+train_values_path = get_config('file_paths.yaml', 'train_values')
+train_labels_path = get_config('file_paths.yaml', 'train_labels')
+print(separated_initial_data_load(train_labels_path, train_labels_path))
